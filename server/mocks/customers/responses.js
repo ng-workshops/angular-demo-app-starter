8d3c33d75cb7bbf6b150c4e69be730a81ce1b608
Andreas Krummsdorf <andreas.krummsdorf@doksafe.de>
2018-07-06 10:04:36 +0200

    Enhance search logic for customers
    thx @Sven :-)

--- server/mocks/customers/responses.js
+++ server/mocks/customers/responses.js
@@ -4,30 +4,40 @@ module.exports = {
   /**
    * @param {string} search
    */
-  getAll: (search) => {
+  getAll: search => {
     if (search) {
-      return customers.filter(p => p.name.toLowerCase().includes(search.toLowerCase())) || [];
+      return (
+        customers.filter(
+          p =>
+            p.name.toLowerCase().includes(search.toLowerCase()) ||
+            p.firstname.toLowerCase().includes(search.toLowerCase()) ||
+            (p.hobbies &&
+              p.hobbies.some(h =>
+                h.toLowerCase().includes(search.toLowerCase())
+              ))
+        ) || []
+      );
     }
 
     return customers;
   },
-  getById: (id) => customers.find(p => p.id === +id),
-  create: (newCustomer) => {
+  getById: id => customers.find(p => p.id === +id),
+  create: newCustomer => {
     let lastCustomer = customers.sort((a, b) => b.id - a.id)[0];
     lastCustomer = lastCustomer || { id: 0 };
-    
+
     newCustomer.id = lastCustomer.id + 1;
     customers.push(newCustomer);
 
     return newCustomer;
   },
-  update: (customer) => {
+  update: customer => {
     let index = customers.findIndex(p => p.id === +customer.id);
     customers.splice(index, 1, { ...customers[index], ...customer });
 
     return customer;
   },
-  delete: (id) => {
+  delete: id => {
     const index = customers.findIndex(i => i.id === +id);
 
     if (index > -1) {
@@ -36,5 +46,4 @@ module.exports = {
 
     return {};
   }
-}
-
+};
